7a32832f9b004fa9ffe25e3c72038ec4100a7cbc
Brian Odida <odida.protas@gmail.com>
2023-02-04 18:47:47 +0300

    setup

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,7 +7,7 @@
 <header>
 	<div class="corner">
 		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+			<!-- <img src={logo} alt="SvelteKit" /> -->
 		</a>
 	</div>
 
@@ -22,8 +22,8 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/define') ? 'page' : undefined}>
+				<a href="/define">Define</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -31,10 +31,93 @@
 		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
+	<div>
+		<!-- component -->
+		<div class="flex justify-cente r pr-6 mt-2">
+			<div class="relative inline-block mb-20">
+				<!-- Dropdown toggle button -->
+				<button
+					class="relative z-10 flex items-center p-2 text-sm text-gray-600 bg-white border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:text-white dark:bg-gray-800 focus:outline-none"
+				>
+					<span class="mx-1">Language</span>
+					<svg
+						class="w-5 h-5 mx-1"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M12 15.713L18.01 9.70299L16.597 8.28799L12 12.888L7.40399 8.28799L5.98999 9.70199L12 15.713Z"
+							fill="currentColor"
+						/>
+					</svg>
+				</button>
+
+				<!-- Dropdown menu -->
+				<div
+					class="absolute right-0 z-20 w-56 py-2 mt-2 overflow-hidden bg-white rounded-md shadow-xl dark:bg-gray-800"
+				>
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						view profile
+					</a>
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Settings
+					</a>
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Keyboard shortcuts
+					</a>
+
+					<hr class="border-gray-200 dark:border-gray-700 " />
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Company profile
+					</a>
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Team
+					</a>
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Invite colleagues
+					</a>
+
+					<hr class="border-gray-200 dark:border-gray-700 " />
+
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Help
+					</a>
+					<a
+						href="#"
+						class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+					>
+						Sign Out
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </header>
 
@@ -45,8 +128,6 @@
 	}
 
 	.corner {
-		width: 3em;
-		height: 3em;
 	}
 
 	.corner a {
---
+++ src/routes/define/+page.svelte
@@ -0,0 +1,69 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="">
+	<h1>Define a word</h1>
+	<p class="mb-6">
+		All words in <a href="/">DHOLUO Dictionary</a> were written by people just like you. Now's your chance
+		to add your own!
+	</p>
+	<p>
+		Please review <a href="/guidelines">DHOLUO Dictionary content guidelines</a> before writing your
+		definition. Here's the short version: Share definitions that other people will find meaningful and
+		never post hate speech or people’s personal information.
+	</p>
+
+	<form class="mt-5">
+		<div class="mb-6">
+			<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Word</label
+			>
+			<input
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Type the word to define"
+				required
+			/>
+		</div>
+		<div class="mb-6">
+			<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Your message</label
+			>
+			<textarea
+				id="message"
+				rows="4"
+				class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Write your thoughts here..."
+			/>
+
+			<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Your password</label
+			>
+			<input
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				required
+				placeholder="Type your definition here"
+			/>
+		</div>
+		<div class="flex items-start mb-6">
+			<div class="flex items-center h-5">
+				<input
+					id="remember"
+					type="checkbox"
+					value=""
+					class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+					required
+				/>
+			</div>
+			<label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Remember me</label
+			>
+		</div>
+		<button
+			type="submit"
+			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+			>Submit</button
+		>
+	</form>
+</div>
