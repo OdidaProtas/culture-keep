24a548f25bbe30d3c98db5ed2dd14786235426e2
Dre <bryodiiidah@gmail.com>
2023-02-10 17:50:21 +0300

    add blog edit

--- src/routes/blog/[slug]/edit/+page.svelte
+++ src/routes/blog/[slug]/edit/+page.svelte
@@ -48,6 +48,10 @@
 			placeholder: 'Write your story...'
 		});
 
+		quill.pasteHTML(data.post.content);
+
+		content = data.post.content;
+
 		quill.on('text-change', function (delta, oldDelta, source) {
 			content = quill.root.innerHTML;
 		});
@@ -118,7 +122,7 @@
 		<button
 			type="submit"
 			class="text-white   bg-blue-700 hover:bg-blue-800 mt-5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-			>Save draft</button
+			>Save changes</button
 		>
 	</form>
 </div>
