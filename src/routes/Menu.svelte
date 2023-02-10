f3c96482e4587d0cbe6e1bd487e759ebe77deb92
Dre <bryodiiidah@gmail.com>
2023-02-10 11:25:15 +0300

    add icon

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -6,11 +6,26 @@
 </script>
 
 <div class="m-2">
-	<button on:click={toggle}>Toggle</button>
+	{#if !menuOpen}
+		<button on:click={toggle}>
+			<svg xmlns="http://www.w3.org/2000/svg" height="36" width="30"
+				><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z" /></svg
+			>
+		</button>
+	{/if}
+	{#if menuOpen}
+		<button on:click={toggle}>
+			<svg xmlns="http://www.w3.org/2000/svg" height="43" width="43"
+				><path
+					d="m16.5 33.6 7.5-7.5 7.5 7.5 2.1-2.1-7.5-7.5 7.5-7.5-2.1-2.1-7.5 7.5-7.5-7.5-2.1 2.1 7.5 7.5-7.5 7.5ZM24 44q-4.1 0-7.75-1.575-3.65-1.575-6.375-4.3-2.725-2.725-4.3-6.375Q4 28.1 4 24q0-4.15 1.575-7.8 1.575-3.65 4.3-6.35 2.725-2.7 6.375-4.275Q19.9 4 24 4q4.15 0 7.8 1.575 3.65 1.575 6.35 4.275 2.7 2.7 4.275 6.35Q44 19.85 44 24q0 4.1-1.575 7.75-1.575 3.65-4.275 6.375t-6.35 4.3Q28.15 44 24 44Zm0-3q7.1 0 12.05-4.975Q41 31.05 41 24q0-7.1-4.95-12.05Q31.1 7 24 7q-7.05 0-12.025 4.95Q7 16.9 7 24q0 7.05 4.975 12.025Q16.95 41 24 41Zm0-17Z"
+				/></svg
+			>
+		</button>
+	{/if}
 </div>
 
 {#if menuOpen}
-	<div class="absolute z-50 top-9 left-2">
+	<div class="absolute z-50 top-18 left-2">
 		<div
 			class="w-48 text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
 		>
