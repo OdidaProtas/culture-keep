34403d9c733b875ceb84b0399523716b138d767d
Dre <bryodiiidah@gmail.com>
2023-02-10 17:14:43 +0300

    enable csr blog form

--- src/routes/blog/new/+page.svelte
+++ src/routes/blog/new/+page.svelte
@@ -13,21 +13,21 @@
 
 	const user = $page.data.session?.user;
 
-	// import { quill } from 'svelte-quill';
+	import { quill } from 'svelte-quill';
 
-	// const options = {
-	// 	modules: {
-	// 		toolbar: [
-	// 			[{ header: [1, 2, 3, 4, 5, 6] }],
-	// 			['bold', 'italic', 'underline', 'strike'],
-	// 			['link', 'image', 'link']
-	// 		]
-	// 	},
-	// 	placeholder: 'Write your story...',
-	// 	theme: 'bubble'
-	// };
+	const options = {
+		modules: {
+			toolbar: [
+				[{ header: [1, 2, 3, 4, 5, 6] }],
+				['bold', 'italic', 'underline', 'strike'],
+				['link', 'image', 'link']
+			]
+		},
+		placeholder: 'Write your story...',
+		theme: 'bubble'
+	};
 
-	// let content;
+	let content;
 </script>
 
 <svelte:head>
@@ -86,20 +86,22 @@
 				<label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-500"
 					>Blog content
 				</label>
-				<textarea
+				<input
+					bind:value={content}
 					id="content"
+					type="hidden"
 					class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					name="content"
 					rows="18"
 					placeholder="Write your post here. Markdown is supported"
 				/>
-				<!-- <div
+				<div
 					class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					use:quill={options}
 					on:text-change={(e) => {
 						content = e.detail.html;
 					}}
-				/> -->
+				/>
 			</div>
 
 			<button
